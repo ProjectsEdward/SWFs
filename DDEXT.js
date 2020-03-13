a515d34dc895c5d51b3dcca9219d5a120fa35b92
ProjectsEdward <49332314+ProjectsEdward@users.noreply.github.com>
2020-03-13 12:14:09 -0500

    Update DDEXT.js

--- DDEXT.js
+++ DDEXT.js
@@ -4,6 +4,29 @@ var game = document.createElement("embed");
 var dropd = document.getElementById("dropd");
 var swffile = dropd.options[dropd.selectedIndex].value;
 
+if (swffile == "ZALTTP"){
+  //setup ZALTTP
+  var flashvars = 
+				{
+					system : 'snes',
+					url : 'https://projectsedward.github.io/SWFs/roms/ZALTTP.smc'
+				};
+				var params = {};
+				var attributes = {};
+				
+				params.allowscriptaccess = 'sameDomain';
+				params.allowFullScreen = 'true';
+				params.allowFullScreenInteractive = 'true';
+				
+				swfobject.embedSWF('https://projectsedward.github.io/SWFs/flash/Nesbox.swf', 'emulator', '1024', '700', '11.2.0', 'https://projectsedward.github.io/SWFs/flash/expressInstall.swf', flashvars, params, attributes);
+        var game = document.getElementById("emulator");
+} else {
+  //src
+var src = document.createAttribute("src");
+src.value = swffile;
+game.setAttributeNode(src);
+} 
+
 //style
 var style = document.createAttribute("style");
 style.value = "position: absolute; opacity: 1; left: 171px; top: 19px;";
@@ -34,14 +57,9 @@ var align = document.createAttribute("align");
 align.value = "center";
 game.setAttributeNode(align);
 
-//src
-var src = document.createAttribute("src");
-src.value = swffile;
-game.setAttributeNode(src);
-
+};
 //Replace game
 var ddiv = document.getElementById("dddiv");
 ddiv.parentNode.replaceChild(game, ddiv);
 game.parentNode.appendChild(game);
-
 }
