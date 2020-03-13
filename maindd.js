af121723d9ce09edc89f19f6f13cbe9a27e7741c
ProjectsEdward <49332314+ProjectsEdward@users.noreply.github.com>
2020-03-13 14:53:42 -0500

    Update maindd.js

--- maindd.js
+++ maindd.js
@@ -1,9 +1,9 @@
 // https://cdn.i-ready.com/instruction/phoenix/release-1.10.x/1/?csid=DI.MATH.NO.8.1000.phx.10_e54f337f-8d0f-42ff-92ba-71ca4efada9e_M_math&type=TUTORIAL#/lesson/math/DI_MATH_NO_8_1000_10
-// https://javascript-minifier.com/
 // https://mrcoles.com/bookmarklet/
 // https://projectsedward.github.io/SWFs/DDEXT.js
-//fetch elements
+
 function maindd(){
+//fetch elements
 var mainDD = document.getElementsByClassName("clssweater-school-bd")[0];
 var video = document.getElementsByTagName("video")[0];
 var musscr = document.getElementsByTagName("script")[37];
