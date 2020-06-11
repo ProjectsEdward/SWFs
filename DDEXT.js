3acd2ec0b67d62ceabe5287b86b5c0a55228712e
meponderR <33591888+meponderR@users.noreply.github.com>
2020-06-11 14:48:11 -0500

    fix

--- DDEXT.js
+++ DDEXT.js
@@ -3,28 +3,30 @@ function setupCall(){
   sha.src = 'https://geraintluff.github.io/sha256/sha256.min.js';
   document.head.appendChild(sha);
 
-  //Create dropdown div and create dropdown
+  //Create dropdown div, create dropdown, and add id
   var dropdiv = document.createElement('div');
   var ddboxtop = (window.innerHeight * 0.02) + 10
   var ddbuttontop = (window.innerHeight * 0.02) + 54
   dropdiv.innerHTML= '<select style="background: #2b99ff;border: 0px solid black;  width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://projectsedward.github.io/SWFs/Doom%20w800%20h500.swf">Doom</option><option value="https://projectsedward.github.io/SWFs/Sports%20Heads%20Basketball.swf">SH Basketball</option><option value="https://projectsedward.github.io/SWFs/Happy%20Wheels.swf">Happy Wheels</option><option value="https://projectsedward.github.io/SWFs/vvvvvv%20w640%20h480.swf">vvvvvv</option><option value="https://projectsedward.github.io/SWFs/Tank_Trouble.swf">Tank Trouble</option><option value="https://projectsedward.github.io/SWFs/ClickerHeroes%20w1000%20h563.swf">Clicker Heroes</option><option value="https://projectsedward.github.io/SWFs/BTD5.swf">BTD5</option><option value="https://projectsedward.github.io/SWFs/SuperMario63.swf">Super Mario 63</option><option value="pswdprot">Protected Content</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=setSWF() type=button>Go</button>';
+  var id = document.createAttribute("id");
+  id.value = 'dddiv';
+  dropdiv.setAttributeNode(id);
 
   //Create reset div and create dropdown button
   var resetdiv= document.createElement('div');
   resetdiv.innerHTML= '<a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 0px; top: 364px; font-family: \'Roboto\', sans-serif;" noreferrer" id="resetbutton" onclick=resetDD() type=button>Reset</a>';
 
-  //add id to div
-  var id = document.createAttribute("id");
-  id.value = 'dddiv';
-  dropdiv.setAttributeNode(id);
 
   //get dropdown divs
   var dropd = document.getElementById("dropd");
   var swfbutton = document.getElementById("swfbutton");
+  
   //set font to Roboto
   var dropstyle= document.createElement('style');
   dropstyle.innerHTML= '@import url(\'https://fonts.googleapis.com/css?family=Roboto:500&display=swap\');';
 
+  var tempdiv = document.getElementById("tempdiv");
+  tempdiv.parentNode.replaceChild(dropdiv, tempdiv);
 }
 
 function resetDD(){
