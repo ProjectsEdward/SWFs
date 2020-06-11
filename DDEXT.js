8392aca74ccedd0a4465594bf5c3d1896a0db9ee
meponderR <33591888+meponderR@users.noreply.github.com>
2020-06-11 12:42:03 -0500

    Add RetroArch and try new way

--- DDEXT.js
+++ DDEXT.js
@@ -1,3 +1,41 @@
+function setupCall(){
+  var sha = document.createElement('script');
+  sha.src = 'https://geraintluff.github.io/sha256/sha256.min.js';
+  document.head.appendChild(sha);
+
+  //Create dropdown div and create dropdown
+  var dropdiv = document.createElement('div');
+  var ddboxtop = (window.innerHeight * 0.02) + 10
+  var ddbuttontop = (window.innerHeight * 0.02) + 54
+  dropdiv.innerHTML= '<select style="background: #2b99ff;border: 0px solid black;  width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://projectsedward.github.io/SWFs/Doom%20w800%20h500.swf">Doom</option><option value="https://projectsedward.github.io/SWFs/Sports%20Heads%20Basketball.swf">SH Basketball</option><option value="https://projectsedward.github.io/SWFs/Happy%20Wheels.swf">Happy Wheels</option><option value="https://projectsedward.github.io/SWFs/vvvvvv%20w640%20h480.swf">vvvvvv</option><option value="https://projectsedward.github.io/SWFs/Tank_Trouble.swf">Tank Trouble</option><option value="https://projectsedward.github.io/SWFs/ClickerHeroes%20w1000%20h563.swf">Clicker Heroes</option><option value="https://projectsedward.github.io/SWFs/BTD5.swf">BTD5</option><option value="https://projectsedward.github.io/SWFs/SuperMario63.swf">Super Mario 63</option><option value="pswdprot">Protected Content</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=setSWF() type=button>Go</button>';
+
+  //Create reset div and create dropdown button
+  var resetdiv= document.createElement('div');
+  resetdiv.innerHTML= '<a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 0px; top: 364px; font-family: \'Roboto\', sans-serif;" noreferrer" id="resetbutton" onclick=resetDD() type=button>Reset</a>';
+
+  //add id to div
+  var id = document.createAttribute("id");
+  id.value = 'dddiv';
+  dropdiv.setAttributeNode(id);
+
+  //get dropdown divs
+  var dropd = document.getElementById("dropd");
+  var swfbutton = document.getElementById("swfbutton");
+  //set font to Roboto
+  var dropstyle= document.createElement('style');
+  dropstyle.innerHTML= '@import url(\'https://fonts.googleapis.com/css?family=Roboto:500&display=swap\');';
+
+}
+
+function resetDD(){
+  var dropdiv = document.getElementById("dddiv");
+  var ddboxtop = (window.innerHeight * 0.02) + 10
+  var ddbuttontop = (window.innerHeight * 0.02) + 54
+  dropdiv.innerHTML= '<select style="background: #2b99ff;border: 0px solid black;  width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://projectsedward.github.io/SWFs/Doom%20w800%20h500.swf">Doom</option><option value="https://projectsedward.github.io/SWFs/Sports%20Heads%20Basketball.swf">SH Basketball</option><option value="https://projectsedward.github.io/SWFs/Happy%20Wheels.swf">Happy Wheels</option><option value="https://projectsedward.github.io/SWFs/vvvvvv%20w640%20h480.swf">vvvvvv</option><option value="https://projectsedward.github.io/SWFs/Tank_Trouble.swf">Tank Trouble</option><option value="https://projectsedward.github.io/SWFs/ClickerHeroes%20w1000%20h563.swf">Clicker Heroes</option><option value="https://projectsedward.github.io/SWFs/BTD5.swf">BTD5</option><option value="https://projectsedward.github.io/SWFs/SuperMario63.swf">Super Mario 63</option><option value="pswdprot">Protected Content</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=setSWF() type=button>Go</button>';
+
+}
+
+
 function setSWF(){
 //get elements
 var game = document.createElement("embed");
@@ -5,7 +43,9 @@ var dropd = document.getElementById("dropd");
 var swffile = dropd.options[dropd.selectedIndex].value;
 
 if (swffile == "pswdprot") {
-  dropd.parentNode.innerHTML= '<input style="border-width: thin; background: #2b99ff; width: 127px; padding: 12px; color: #fff; border: 0px solid black; position: absolute; opacity: 1; left: 175px; top: 24px; font-family: \'Roboto\', sans-serif;" id="dropd" placeholder= \'Password\'><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 332px; top: 24px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=setpswd() type=button>Go</button><script></script>';
+  var ddboxtop = (window.innerHeight * 0.02) + 10
+  var ddbuttontop = (window.innerHeight * 0.02) + 54
+  dropd.parentNode.innerHTML= '<input style="border-width: thin; background: #2b99ff; width: 127px; padding: 12px; color: #fff; border: 0px solid black; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd" placeholder= \'Password\'><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=setpswd() type=button>Go</button><script></script>';
 
 } else {
 //style
@@ -54,8 +94,11 @@ ddiv.appendChild(game);
 
 function setpswd(){
   var dropd = document.getElementById("dropd");
-  if (sha256(dropd.value) == "b7fb0394c7183fd5cac17fb41961c826212a185070e4c1d2f4920e51c1dee35f")
-  dropd.parentNode.innerHTML= '<select style="background: #2b99ff; border: 0px solid black; width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: 24px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://app.parsecgaming.com/">Parsec</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 332px; top: 24px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=framer() type=button>Go</button>';
+  if (sha256(dropd.value) == "b7fb0394c7183fd5cac17fb41961c826212a185070e4c1d2f4920e51c1dee35f") {
+  var ddboxtop = (window.innerHeight * 0.02) + 10
+  var ddbuttontop = (window.innerHeight * 0.02) + 54
+  dropd.parentNode.innerHTML= '<select style="background: #2b99ff; border: 0px solid black; width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://app.parsecgaming.com/">Parsec</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 332px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=framer() type=button>Go</button>';
+}
 }
 
 function framer(){
---
+++ setup.js
@@ -0,0 +1,29 @@
+// https://cdn.i-ready.com/instruction/phoenix/release-1.10.x/1/?csid=DI.MATH.NO.8.1000.phx.10_e54f337f-8d0f-42ff-92ba-71ca4efada9e_M_math&type=TUTORIAL#/lesson/math/DI_MATH_NO_8_1000_10
+// https://mrcoles.com/bookmarklet/
+// https://projectsedward.github.io/SWFs/DDEXT.js
+
+function setup(url){
+//fetch elements
+var mainElement = document.getElementsByClassName("clssweater-school-bd")[0];
+var video = document.getElementsByTagName("video")[0];
+var musscr = document.getElementsByTagName("script")[37];
+var body = document.getElementsByTagName("body")[0];
+
+var imported = document.createElement('script');
+imported.src = url;
+document.head.appendChild(imported);
+
+//create temp div
+var tempdiv = document.createElement('div');
+var id = document.createAttribute("id");
+id.value = 'tempdiv';
+tempdiv.setAttributeNode(id);
+
+
+//remove video, replace with temp div
+video.parentNode.removeChild(video);
+musscr.parentNode.removeChild(musscr);
+body.appendChild(resetdiv);
+mainDD.parentNode.replaceChild(tempdiv, mainElement);
+setupCall();
+}
