f65f1522b2d2f6640c1243835c55238df7b8204a
meponderR <33591888+meponderR@users.noreply.github.com>
2020-06-20 15:35:01 -0500

    DD v3.2

--- DDEXT.js
+++ DDEXT.js
@@ -11,7 +11,7 @@ function setupCall(){
   var dropdiv = document.createElement('div');
   var ddboxtop = (window.innerHeight * 0.02) + 10
   var ddbuttontop = (window.innerHeight * 0.02) + 54
-  dropdiv.innerHTML= '<select style="background: #2b99ff;border: 0px solid black;  width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://projectsedward.github.io/SWFs/Doom%20w800%20h500.swf">Doom</option><option value="https://projectsedward.github.io/SWFs/Sports%20Heads%20Basketball.swf">SH Basketball</option><option value="https://projectsedward.github.io/SWFs/Happy%20Wheels.swf">Happy Wheels</option><option value="https://projectsedward.github.io/SWFs/vvvvvv%20w640%20h480.swf">vvvvvv</option><option value="https://projectsedward.github.io/SWFs/Tank_Trouble.swf">Tank Trouble</option><option value="https://projectsedward.github.io/SWFs/ClickerHeroes%20w1000%20h563.swf">Clicker Heroes</option><option value="https://projectsedward.github.io/SWFs/BTD5.swf">BTD5</option><option value="https://projectsedward.github.io/SWFs/SuperMario63.swf">Super Mario 63</option><option value="pswdprot">Protected Content</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=buttonSet() type=button>Go</button>';
+  dropdiv.innerHTML= '<select style="background: #2b99ff;border: 0px solid black;  width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="flasher("https://projectsedward.github.io/SWFs/Doom%20w800%20h500.swf")">Doom</option><option value="flasher("https://projectsedward.github.io/SWFs/Sports%20Heads%20Basketball.swf")">SH Basketball</option><option value="flasher("https://projectsedward.github.io/SWFs/Happy%20Wheels.swf")">Happy Wheels</option><option value="flasher("https://projectsedward.github.io/SWFs/vvvvvv%20w640%20h480.swf")">vvvvvv</option><option value="flasher("https://projectsedward.github.io/SWFs/Tank_Trouble.swf")">Tank Trouble</option><option value="flasher("https://projectsedward.github.io/SWFs/ClickerHeroes%20w1000%20h563.swf")">Clicker Heroes</option><option value="flasher("https://projectsedward.github.io/SWFs/BTD5.swf")">BTD5</option><option value="flasher("https://projectsedward.github.io/SWFs/SuperMario63.swf")">Super Mario 63</option><option value="pswdprot">Protected Content</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=buttonSet() type=button>Go</button>';
   var id = document.createAttribute("id");
   id.value = 'dddiv';
   dropdiv.setAttributeNode(id);
@@ -41,8 +41,12 @@ function setupCall(){
 function buttonSet(){
   var dropd = document.getElementById("dropd");
   var code = dropd.options[dropd.selectedIndex].value;
+
+  if (code == pswdprot) {
+
+  } else {
   eval(code);
-}
+}}
 
 //Resetup
 function resetDD(){
@@ -69,7 +73,7 @@ function setpswd(){
   if (sha256(dropd.value) == "b7fb0394c7183fd5cac17fb41961c826212a185070e4c1d2f4920e51c1dee35f") {
   var ddboxtop = (window.innerHeight * 0.02) + 10
   var ddbuttontop = (window.innerHeight * 0.02) + 54
-  dropd.parentNode.innerHTML= '<select style="background: #2b99ff; border: 0px solid black; width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="https://app.parsecgaming.com/">Parsec</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 332px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=framer() type=button>Go</button>';
+  dropd.parentNode.innerHTML= '<select style="background: #2b99ff; border: 0px solid black; width: 152px; padding: 11px; color: #fff; position: absolute; opacity: 1; left: 175px; top: ' + ddboxtop.toString() + 'px; font-family: \'Roboto\', sans-serif;" id="dropd"><option>  </option><option value="framer("https://app.parsecgaming.com/")">Parsec</option></select><a style="cursor: pointer; border-width: thin; background: #2b99ff; padding: 12px; color: #fff; position: absolute; opacity: 1; left: 332px; top: ' + ddbuttontop.toString() + 'px; font-family: \'Roboto\', sans-serif;" noreferrer" id="swfbutton" onclick=framer() type=button>Go</button>';
 }
 }
 
