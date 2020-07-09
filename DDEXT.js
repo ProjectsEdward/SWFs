b0e9a617a242da4f1b49d4f32d4966b1872d679a
meponderR <33591888+meponderR@users.noreply.github.com>
2020-07-09 15:58:36 -0500

    DD v3.17

--- DDEXT.js
+++ DDEXT.js
@@ -226,9 +226,10 @@ function framerFullWidth(url){
 }
 
 // Play Music
-document.addEventListener ("keydown", function (zEvent) {
+/* document.addEventListener ("keydown", function (zEvent) {
     if (zEvent.key === "u"  &&  zEvent.key === "r"  &&  zEvent.key === "d") {
       new Audio('https://projectsedward.github.io/SWFs/Song.mp3').play()
 
     }
 } );
+*/
